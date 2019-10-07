bcc16f154430f72024577a9717227c4459ea6f6d
“Aliaksandr <aliaksandr.yanushkevich@gmail.com>
2019-10-08 02:03:18 +0300

    dкфае: problem with j in '1' case

--- src/index.js
+++ src/index.js
@@ -39,8 +39,48 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
-}
+// MY CODE BELOW
+    let str = '000000001100101010100000000010**********';
+    let inputArr = [];
+    for (let i = 0; i < str.length; i += 10) {
+      inputArr.push(str.substr(i,10));
+    }
+    console.log(inputArr);
+    function decoder(inputArr) {
+      let str = '';
+      let morse = [];
+      
+      for (let i = 0; i < inputArr.length; i++) {
+        for (let j = 0; j < inputArr[i].length; j += 2) {
+          let letter = '';
+          switch(inputArr[i][j]) {
+            case '0':
+              break;
+            case '*':
+              morse.push(' ');
+              j += 10;
+              break;
+            case '1':
+              for (let n = j; n < inputArr[i].length; n += 2) {
+                if (inputArr[i][n+1] === '0') {
+                  letter += '.';
+                } else if (inputArr[i][n+1] === '1') {
+                  letter += '-';
+                }
+              }
+          morse.push(letter);
+          console.log('i = ' + i +' j=' + j);
+          console.log('letter=' + letter);
+          break;
+          }
+        }
+      }
+    }
+    console.log(decoder(inputArr));
 
+    
+}
+// MY CODE ABOVE
 module.exports = {
     decode
 }
